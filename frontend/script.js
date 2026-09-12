ef02506287e78bdfb2602c1d0da82253851462c9
AbdulRaheem Saleem <164777307+Raheem-12@users.noreply.github.com>
2026-09-12 16:16:12 -0400

    Fix phone number formatting

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,26 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 
-const phoneInput = document.querySelector('input[type="tel"]');
+//Phone section
+const phoneInput = document.getElementById("phone");
 
 if (phoneInput) {
     phoneInput.addEventListener("input", function () {
-        const digits = this.value.replace(/\D/g, "").slice(0, 10);
+        const digits = phoneInput.value.replace(/\D/g, "").slice(0, 10);
 
-        if (digits.length <= 3) {
-            this.value = digits;
-        } else if (digits.length <= 6) {
-            this.value = `(${digits.slice(0, 3)}) ${digits.slice(3)}`;
-        } else {
-            this.value =
-                `(${digits.slice(0, 3)}) ${digits.slice(3, 6)}-${digits.slice(6)}`;
+        let formattedNumber = "";
+
+        if (digits.length > 0) {
+            formattedNumber = `(${digits.slice(0, 3)}`;
         }
+
+        if (digits.length >= 4) {
+            formattedNumber += `)-${digits.slice(3, 6)}`;
+        }
+
+        if (digits.length >= 7) {
+            formattedNumber += `-${digits.slice(6, 10)}`;
+        }
+
+        phoneInput.value = formattedNumber;
     });
 }
